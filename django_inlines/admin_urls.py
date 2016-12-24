adafba66191cded0db4ddc48f40d8da797dd5d55
Steve Kossouho <steve.kossouho@gmail.com>
2016-12-24 21:19:38 +0100

    Django 1.10 support

--- django_inlines/admin_urls.py
+++ django_inlines/admin_urls.py
@@ -3,8 +3,10 @@ try:
 except:
     from django.conf.urls import *
 
+from django_inlines.views import js_inline_config, get_inline_form
 
-urlpatterns = patterns('django_inlines.views',
-    url(r'^inline_config\.js$', 'js_inline_config', name='js_inline_config'),
-    url(r'^get_inline_form/$', 'get_inline_form', name='get_inline_form'),
-)
+
+urlpatterns = [
+    url(r'^inline_config\.js$', js_inline_config, name='js_inline_config'),
+    url(r'^get_inline_form/$', get_inline_form, name='get_inline_form'),
+]
